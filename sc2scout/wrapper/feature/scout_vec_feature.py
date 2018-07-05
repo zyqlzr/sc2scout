dd36e8fe1a2e6c76c8b3c747bcfda4cc1accfdfe
zakzheng <zyqlzr@gmail.com>
2018-07-05 16:48:14 +0800

    refactor feature

--- sc2scout/wrapper/feature/scout_vec_feature.py
+++ sc2scout/wrapper/feature/scout_vec_feature.py
@@ -5,6 +5,48 @@ from sc2scout.wrapper.util.dest_range import DestRange
 SCOUT_IN_RANGE = 1
 SCOUT_OUT_RANGE = 0
 
+class ScoutSimpleFeature(FeatureExtractor):
+    def __init__(self):
+        self._reverse = False
+        self._map_size = None
+        self._env = None
+
+    def reset(self, env):
+        self.env = env
+        self._map_size = self.env.unwrapped.map_size()
+        self._reverse = self._judge_reverse(env)
+
+    def obs_space(self):
+        low = np.zeros(6)
+        high = np.ones(6)
+        return Box(low, high)
+
+    def _judge_reverse(self, env):
+        scout = self.env.unwrapped.scout()
+        if scout.float_attr.pos_x < scout.float_attr.pos_y:
+            return False
+        else:
+            return True
+
+    def _pos_transfer(self, x, y):
+        if not self._reverse:
+            return (x, y)
+        cx = self._map_size[0] / 2
+        cy = self._map_size[1] / 2
+        pos_x = 0.0
+        pos_y = 0.0
+        if x > cx:
+            pos_x = cx - abs(x - cx)
+        else:
+            pos_x = cx + abs(x - cx)
+
+        if y > cy:
+            pos_y = cy - abs(y - cy)
+        else:
+            pos_y = cy + abs(y - cy)
+
+        return (pos_x, pos_y)
+
 class ScoutVecFeature(FeatureExtractor):
     def __init__(self):
         super(ScoutVecFeature, self).__init__()
