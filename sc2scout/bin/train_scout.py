27d784e25538948aebb8488d4aaf6e17739b269c
scutzck033 <404762007@qq.com>
2018-07-10 17:44:50 +0800

    zyqlzr version

--- sc2scout/bin/train_scout.py
+++ sc2scout/bin/train_scout.py
@@ -3,8 +3,9 @@ from baselines import logger
 
 from pysc2 import maps
 from pysc2.env import sc2_env
-from sc2scout.envs import ZergScoutEnv,ZergEvadeEnv
+from sc2scout.envs import ZergScoutEnv
 from sc2scout.wrapper.wrapper_factory import make, model
+from sc2scout.wrapper.util.sc2_params import races, difficulties
 
 from absl import app
 from absl import flags
@@ -12,35 +13,31 @@ import time
 
 FLAGS = flags.FLAGS
 flags.DEFINE_bool("render", True, "Whether to render with pygame.")
-flags.DEFINE_integer("screen_resolution", 64,
+
+flags.DEFINE_integer("screen_resolution", 84,
                      "Resolution for screen feature layers.")
 flags.DEFINE_integer("minimap_resolution", 64,
                      "Resolution for minimap feature layers.")
+flags.DEFINE_float("screen_ratio", "1.33",
+                   "Screen ratio of width / height")
+flags.DEFINE_string("agent_interface_format", "feature",
+                    "Agent Interface Format: [feature|rgb]")
 
 flags.DEFINE_integer("max_agent_episodes", 1, "Total agent episodes.")
 flags.DEFINE_integer("max_step", 10000, "Game steps per episode.")
-flags.DEFINE_integer("step_mul", 1, "Game steps per agent step.")
-# init 8 since sc2_env initilization period will class _step() function once, each time 8*4(1+3_skip) steps taken
+flags.DEFINE_integer("step_mul", 8, "Game steps per agent step.")
 flags.DEFINE_integer("random_seed", None, "Random_seed used in game_core.")
 
-flags.DEFINE_string("train_log_dir", './log/evade_log', "train log directory")
-flags.DEFINE_string("checkpoint_path", './model_save/evade_model/model6/', "load saved model")
+flags.DEFINE_string("train_log_dir", './log', "train log directory")
+flags.DEFINE_string("checkpoint_path", './model_save', "load saved model")
 flags.DEFINE_integer("checkpoint_freq", 5000, "load saved model")
-flags.DEFINE_string("agent", "pysc2.agents.random_agent.RandomAgent",
-                    "Which agent to run")
-flags.DEFINE_string("agent_config", "",
-                    "Agent's config in py file. Pass it as python module."
-                    "E.g., tstarbot.agents.dft_config")
-flags.DEFINE_string("wrapper", 'evade_v0', "the name of wrapper")
-flags.DEFINE_enum("agent_race", 'Z', sc2_env.races.keys(), "Agent's race.")
-flags.DEFINE_enum("bot_race", 'Z', sc2_env.races.keys(), "Bot's race.")
-flags.DEFINE_enum("difficulty", None, sc2_env.difficulties.keys(),
-                  "Bot's strength.")
+flags.DEFINE_string("wrapper", None, "the name of wrapper")
+flags.DEFINE_enum("agent_race", 'Z', races.keys(), "Agent's race.")
+flags.DEFINE_enum("bot_race", 'Z', races.keys(), "Bot's race.")
+flags.DEFINE_string("difficulty", '9', "Bot's strength.")
 
 flags.DEFINE_bool("disable_fog", False, "Turn off the Fog of War.")
-flags.DEFINE_bool("profile", False, "Whether to turn on code profiling.")
-flags.DEFINE_bool("trace", False, "Whether to trace the code execution.")
-flags.DEFINE_integer("parallel", 2, "How many instances to run in parallel.")
+
 flags.DEFINE_integer("max_episode_rwd", None, 'the max sum reward of episode')
 flags.DEFINE_bool("save_replay", True, "Whether to save a replay at the end.")
 
@@ -49,7 +46,7 @@ flags.DEFINE_integer('param_bf', 50000, 'buffer size')
 flags.DEFINE_float('param_ef', 0.1, 'explore_fraction')
 flags.DEFINE_float('param_efps', 0.02, 'exploration_final_eps')
 
-flags.DEFINE_string("map", 'scout_evade', "Name of a map to use.")
+flags.DEFINE_string("map", None, "Name of a map to use.")
 flags.mark_flag_as_required("map")
 flags.mark_flag_as_required("wrapper")
 
@@ -60,13 +57,6 @@ def callback(lcl, _glb):
     # stop training if reward exceeds 199
     step = lcl['t']
     reset_flag = lcl['reset']
-    '''
-    if reset_flag:
-        global last_done_step
-        logger.log('last_done_step={} step={} last_episode_rwd={}'.format(
-                last_done_step, step, lcl['episode_rewards'][-2:-1]))
-        last_done_step = step
-    '''
 
     if reset_flag:
         global last_done_step
@@ -97,36 +87,34 @@ def main(unused_argv):
 
     logger.configure(dir=FLAGS.train_log_dir, format_strs=['log'])
 
-    # env = ZergScoutEnv(
-    #         map_name=FLAGS.map,
-    #         agent_race=FLAGS.agent_race,
-    #         bot_race=FLAGS.bot_race,
-    #         difficulty=FLAGS.difficulty,
-    #         step_mul=FLAGS.step_mul,
-    #         random_seed=rs,
-    #         game_steps_per_episode=FLAGS.max_step,
-    #         screen_size_px=(FLAGS.screen_resolution, FLAGS.screen_resolution),
-    #         minimap_size_px=(FLAGS.minimap_resolution, FLAGS.minimap_resolution),
-    #         score_index=-1,  # this indicates the outcome is reward
-    #         disable_fog=FLAGS.disable_fog,
-    #         visualize=FLAGS.render
-    #     )
+    players = []
+    players.append(sc2_env.Bot(races[FLAGS.bot_race], difficulties[FLAGS.difficulty]))
+    players.append(sc2_env.Agent(races[FLAGS.agent_race]))
 
+    screen_res = (int(FLAGS.screen_ratio * FLAGS.screen_resolution) // 4 * 4,
+                  FLAGS.screen_resolution)
+    if FLAGS.agent_interface_format == 'feature':
+        agent_interface_format = sc2_env.AgentInterfaceFormat(
+        feature_dimensions = sc2_env.Dimensions(screen=screen_res,
+            minimap=FLAGS.minimap_resolution))
+    elif FLAGS.agent_interface_format == 'rgb':
+        agent_interface_format = sc2_env.AgentInterfaceFormat(
+        rgb_dimensions=sc2_env.Dimensions(screen=screen_res, 
+            minimap=FLAGS.minimap_resolution))
+    else:
+        raise NotImplementedError
 
-    env = ZergEvadeEnv(
-        map_name=FLAGS.map,
-        agent_race=FLAGS.agent_race,
-        bot_race=FLAGS.bot_race,
-        difficulty=FLAGS.difficulty,
-        step_mul=FLAGS.step_mul,
-        random_seed=rs,
-        game_steps_per_episode=FLAGS.max_step,
-        screen_size_px=(FLAGS.screen_resolution, FLAGS.screen_resolution),
-        minimap_size_px=(FLAGS.minimap_resolution, FLAGS.minimap_resolution),
-        score_index=-1,  # this indicates the outcome is reward
-        disable_fog=FLAGS.disable_fog,
-        visualize=FLAGS.render
-    )
+    env = ZergScoutEnv(
+            map_name=FLAGS.map,
+            players=players,
+            step_mul=FLAGS.step_mul,
+            random_seed=rs,
+            game_steps_per_episode=FLAGS.max_step,
+            agent_interface_format=agent_interface_format,
+            score_index=-1,  # this indicates the outcome is reward
+            disable_fog=FLAGS.disable_fog,
+            visualize=FLAGS.render
+        )
 
     env = make(FLAGS.wrapper, env)
 
@@ -154,3 +142,4 @@ def entry_point():  # Needed so setup.py scripts work.
 
 if __name__ == "__main__":
     app.run(main)
+
