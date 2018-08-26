6dde4616b522cee110f7a1860f27005d1aee038c
zakzheng <zyqlzr@gmail.com>
2018-08-26 15:07:10 +0800

    modify backward_rwd

--- sc2scout/wrapper/reward/backward_reward.py
+++ sc2scout/wrapper/reward/backward_reward.py
@@ -1,3 +1,4 @@
+from sc2scout.envs import scout_macro as sm
 from sc2scout.wrapper.reward.reward import Reward
 from sc2scout.wrapper.util.dest_range import DestRange
 
@@ -5,17 +6,37 @@ class BackwardRwd(Reward):
     def __init__(self, weight=1):
         super(BackwardRwd, self).__init__(weight)
         self._dest = None
+        self._last_dist = None
+        self._home = None
 
     def reset(self, obs, env):
-        home = env.unwrapped.owner_base()
-        self._dest = DestRange(home, dest_range=16)
+        scout = env.unwrapped.scout()
+        self._home = env.unwrapped.owner_base()
+        self._dest = DestRange(self._home, dest_range=16)
+        self._last_dist = None
 
     def compute_rwd(self, obs, reward, done, env):
         scout = env.unwrapped.scout()
-        if self._dest.in_range((scout.float_attr.pos_x, scout.float_attr.pos_y)):
-            self.rwd = 0
+        if self._last_dist is None:
+            self._last_dist = self._compute_dist(scout)
+
+        if not self._dest.in_range((scout.float_attr.pos_x, 
+                                    scout.float_attr.pos_y)):
+            tmp_dist = self._compute_dist(scout)
+            if tmp_dist > self._last_dist:
+                self.rwd = self.w * -1
+            else:
+                self.rwd = 0
+            self._last_dist = tmp_dist
         else:
-            self.rwd = -1 * self.w
+            self.rwd = 0
+        print('backreward=', self.rwd)
+
+    def _compute_dist(self, scout):
+        dist = sm.calculate_distance(scout.float_attr.pos_x, 
+                                     scout.float_attr.pos_y, 
+                                     self._home[0], self._home[1])
+        return dist
 
 class BackwardFinalRwd(Reward):
     def __init__(self, weight=50):
